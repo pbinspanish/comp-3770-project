5f748235fce9243fd75d2debffabb8969769b917
Josiah Henson <pbinspanish@outlook.com>
2023-12-06 20:33:46 -0500

    document main menu controller script

--- Assets/UI/Scripts/MainMenuController.cs
+++ Assets/UI/Scripts/MainMenuController.cs
@@ -2,21 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Contains definitions for methods called by buttons in the main menu.
+/// </summary>
 public class MainMenuController : MonoBehaviour
 {
-    public GameObject menuPrefab;
-    public GameObject playerPrefab; 
-    public Transform playerStart;
+    #region Variables
 
+    public GameObject mainMenu;             // main menu object to be destroyed on start
+    public GameObject playerPrefab;         // player prefab to spawn on game start
+    public Transform playerStartPosition;   // position and angle to spawn the player at
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Destroys the menu and spawns the player on game start.
+    /// </summary>
     public void StartGameButton_OnPress()
     {
-        Instantiate(playerPrefab, playerStart.position, playerStart.rotation);
-        
-        Destroy(menuPrefab);
+        Instantiate(playerPrefab, playerStartPosition.position, playerStartPosition.rotation);
+
+        Destroy(mainMenu);
     }
 
+    /// <summary>
+    /// Quits the application on game exit.
+    /// </summary>
     public void ExitGameButton_OnPress()
     {
         Application.Quit();
     }
+
+    #endregion
 }
