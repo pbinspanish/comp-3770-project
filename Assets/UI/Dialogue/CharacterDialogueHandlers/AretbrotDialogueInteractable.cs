64eb5916fa29e28e579b3df03cc4a68cdcb1a806
bilalmaa <146762263+bilalmaa@users.noreply.github.com>
2023-12-11 16:44:38 -0500

    jeez

--- Assets/UI/Dialogue/CharacterDialogueHandlers/AretbrotDialogueInteractable.cs
+++ Assets/UI/Dialogue/CharacterDialogueHandlers/AretbrotDialogueInteractable.cs
@@ -23,6 +23,7 @@ public class AretbrotDialogueInteractable : DialogueInteractable
         aretbrot.GetComponent<Animator>().SetBool("DieBitch", true);
         Destroy(this);
         GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMove>().hasSword = true;
+        GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<MeshRenderer>().enabled = true;
         GameObject.FindGameObjectWithTag("Player").GetComponent<DialogueInitiator>().currentDialogueInteractable = null;
         GameObject.FindGameObjectWithTag("Player").GetComponent<DialogueInitiator>().interactionIndicator.SetActive(false);
     }
